71c2e93dc65035f9d85950f7390981bada96e366
Marine Tomazi <m.tomazi@hotmail.fr>
2020-10-22 19:55:20 +0200

    Iteration 2 done

--- src/App.js
+++ src/App.js
@@ -1,55 +1,16 @@
 import React from 'react';
+import Navbar from './navbar/Navbar';
+import FormField from './formfield/FormFields';
 import 'bulma/css/bulma.css';
 
 const App = () => {
-  return (<nav className="navbar is-transparent">
-  <div className="navbar-brand">
-    <a className="navbar-item" href="https://bulma.io">
-      <img src="https://bulma.io/images/bulma-logo.png" alt="Bulma: a modern CSS framework based on Flexbox" width="112" height="28"/>
-    </a>
-    <div className="navbar-burger burger" data-target="navbarExampleTransparentExample">
-      <span></span>
-      <span></span>
-      <span></span>
+  return(
+    <div>
+      <Navbar/>
+      <FormField label="Name" type="text" placeholder="e.g Alex Smith" />
+      <FormField label="Email" type="email" placeholder="e.g. alexsmith@gmail.com" />
     </div>
-  </div>
-
-  <div id="navbarExampleTransparentExample" className="navbar-menu">
-    <div className="navbar-start">
-      <a className="navbar-item" href="https://bulma.io/">
-        Home
-      </a>
-    </div>
-
-    <div className="navbar-end">
-      <div className="navbar-item">
-        <div className="field is-grouped">
-          <p className="control">
-            <a className="bd-tw-button button" data-social-target="https://bulma.io" target="_blank" href="https://twitter.com/intent/tweet?text=Bulma: a modern CSS framework based on Flexbox&amp;hashtags=bulmaio&amp;url=https://bulma.io&amp;via=jgthms">
-              <span>
-                Login
-              </span>
-            </a>
-          </p>
-          <p className="control">
-            <a className="button is-primary" href="https://github.com/jgthms/bulma/releases/download/0.9.1/bulma-0.9.1.zip">
-              <span>Signup</span>
-            </a>
-          </p>
-        </div>
-      </div>
-    </div>
-  </div>
-</nav>)
-};
-
-
-         
-             
-    
-
-
-
-
+  )
+}
 
 export default App;
