9eeb735774eb10b1f8824515432e6d2c1818bcdf
Lekmiti Wafaa <lekmiti.wafaa@gmail.com>
2020-10-22 19:49:19 +0200

    iteration 2 done

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 import React from 'react';
 import Navbar from './navbar/Navbar.js';
+import FormField from './formfield/FormField.js'
 import 'bulma/css/bulma.css';
 
 const App = () => {
    return (
       <div>
+
+         <div>
          <Navbar />
+         </div>
+
+         <div>  
+         <FormField label="Name" type="text" placeholder="e.g Alex Smith" />
+         <FormField label="Email" type="email" placeholder="e.g. alexsmith@gmail.com" />
+         </div>
+
       </div>
+      
    );
 };
 
