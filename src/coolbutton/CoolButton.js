b7075df0152bcc833319d871c9241d262e985968
Marine Tomazi <m.tomazi@hotmail.fr>
2020-10-22 20:56:35 +0200

    iteration 3 done

--- src/coolbutton/CoolButton.js
+++ src/coolbutton/CoolButton.js
@@ -2,10 +2,26 @@ import React from 'react';
 import 'bulma/css/bulma.css';
 
 const CoolButton = props => {
-    return(<div className="buttons">
-    <button className="button is-success">{props.children}</button>
-    <button className="button is-danger is-rounded">{props.children}</button>
-  </div>)
+
+  const classes = ['button']
+  classes.push(props.className)
+
+  console.log('props=', props)
+  if (props.isSmall=== true) {
+    // rajouter la classe 'is-small' ^
+    classes.push('is-small')
+  }
+
+  if (props.isSuccess === true) {
+    // rajouter la classe 'is-success' 
+    classes.push('is-success')
+  }else{
+    classes.push('is-danger')
+  }
+
+  return (
+    <button className={classes.join(' ')}>{props.children}</button>
+  )
 }
 
 export default CoolButton;
