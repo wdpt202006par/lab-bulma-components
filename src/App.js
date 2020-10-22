138e6a50afc8562e2a5ca4fcba9ef3a33103f1c3
Pierre-Alderic Boegler <pa.boegler@gmail.com>
2020-10-22 21:09:15 +0200

    iteration 3 done

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import Navbar from './navbar/Navbar';
 import FormField from './formfield/FormField';
+import CoolButton from './coolbutton/CoolButton';
 
 const App = () => {
   return (
     <div>
       <Navbar />
       <FormField label="Name" type="text" placeholder="e.g Alex Smith" />
-  <FormField label="Email" type="email" placeholder="TOTO e.g. alexsmith@gmail.com" />
+      <FormField label="Email" type="email" placeholder="TOTO e.g. alexsmith@gmail.com" />
+      <CoolButton isSmall isDanger className="is-rounded my-class">Button 1</CoolButton>
+      <CoolButton isSmall isSuccess>Button 2</CoolButton>
     </div>
   )
 };
--- src/formfield/FormField.js
+++ src/formfield/FormField.js
@@ -1,16 +1,16 @@
 import React from 'react';
 
 class FormField extends React.Component {
-    render(){
-        return(<div>
-<div class="field">
-  <label class="label">{this.props.label}</label>
-  <div class="control">
-    <input class="input" type={this.props.type} placeholder={this.props.placeholder} />
-  </div>
-</div>
-        </div>)
-    }
+  render() {
+    return (<div>
+      <div className="field">
+        <label className="label">{this.props.label}</label>
+        <div className="control">
+          <input className="input" type={this.props.type} placeholder={this.props.placeholder} />
+        </div>
+      </div>
+    </div>)
+  }
 }
 
 export default FormField;
