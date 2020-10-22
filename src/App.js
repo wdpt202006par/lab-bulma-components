bdcff02743ce485fd10dddc575481c39c5ff8c11
Nina Quaresma <quaresma.nina@gmail.com>
2020-10-22 19:36:44 +0200

    Iteration 2 ok

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Navbar from './navbar/Navbar';
+import FormField from './formfield/FormField';
 
 const App = () => {
   return (
     <div>
       <Navbar />
-
+      <FormField label="Name" type="text" placeholder="e.g Alex Smith" />
+  <FormField label="Email" type="email" placeholder="TOTO e.g. alexsmith@gmail.com" />
     </div>
   )
 };
