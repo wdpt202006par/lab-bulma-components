71be2dd018e961a82564f2f9fb138845d2387e4b
Enine Chareunsouk <enine.chareunsouk@gmail.com>
2020-10-22 19:32:34 +0200

    iterations 1 et 2 done

---
+++ src/formfield/FormField.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+const Formfield = (props) => {
+    return (
+        <div>
+            <div className="field">
+                <label className="label">{props.label}</label>
+                <div className="control">
+                    <input className="input" type={props.type} placeholder={props.placeholder} />
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Formfield;
+
+
+
+
+
+
+
+
