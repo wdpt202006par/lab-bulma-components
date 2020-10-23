48ad3038c08b4d2a83c24456ecc86f32181b65ea
toufiq23 <drrameraaron023@gmail.com>
2020-10-23 02:50:02 +0200

    all done

--- src/App.js
+++ src/App.js
@@ -1,7 +1,32 @@
 import React from 'react';
+import Navbar from "./navbar/Navbar";
+import FormField from "./formfield/FormField";
+import Signup from "./signup/Signup";
+import Message from './message/Message';
 
 const App = () => {
-  return <h1>Bulma</h1>;
+	return(
+		<div>
+			<Navbar />
+			<FormField
+				label="Name"
+				name="User's name"
+				type="text"
+				placeholder="e.g Alex Smith"
+			/>
+      <FormField
+        label="Email"
+        name="User's email"
+        type="email"
+        placeholder="e.g. alexsmith@gmail.com"
+      />
+			<Message isInfo title="Hello World">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit.{" "}
+        <strong>Pellentesque risus mi</strong>.
+      </Message>
+			<Signup />
+		</div>
+	)
 };
 
 export default App;
